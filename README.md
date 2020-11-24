3c05044927e93a49d658c7033c7bafd2b524505c
Nathan Camilleri <nathan.camilleri@epitech.eu>
2020-11-24 17:11:08 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# twitch-chatbot-leaderboard
+ My first twitch chatbot that gets the leaderboard of several games on speedrun.com
---
+++ src/app.js
@@ -0,0 +1,87 @@
+import tmi from 'tmi.js';
+import {convertHMS} from './tools';
+
+const dotenv = require('dotenv');
+dotenv.config();
+const axios = require('axios');
+
+let timeTopPlayer = [];
+let idTopPlayer = [];
+let nameTopPlayer = [];
+
+async function gameLeaderboard(url) {
+    try {
+        const response = await axios.get(url);
+        const json = await response.data.data.runs;
+
+        await parseThreeTopPlayer(json);
+        await searchNamePlayerById();
+    } catch (error) {
+        console.log('error', error);
+    }
+}
+
+function parseThreeTopPlayer(json) {
+    idTopPlayer.length = 0;
+    timeTopPlayer.length = 0;
+
+    let indexArray = 0;
+
+    while (indexArray <= 2) {
+        timeTopPlayer.push(convertHMS(json[indexArray].run.times.primary_t));
+        idTopPlayer.push(json[indexArray].run.players[0].id);
+        indexArray++;
+    }
+}
+
+async function searchNamePlayerById() {
+    nameTopPlayer.length = 0;
+
+    let indexArray = 0;
+
+    while (indexArray <= 2) {
+        try {
+            const response = await axios.get(
+                'https://www.speedrun.com/api/v1/users/' +
+                idTopPlayer[indexArray]);
+            const json = await response.data.data.names.international;
+            nameTopPlayer.push(json);
+            indexArray++;
+        } catch (error) {
+            console.log('error', error);
+        }
+    }
+}
+
+const options = {
+    options: {debug: true, messagesLogLevel: 'info'},
+    connection: {
+        reconnect: true,
+        secure: true,
+    },
+    identity: {
+        username: process.env.BOTNAME,
+        password: process.env.OAUTHTOKEN,
+    },
+    channels: process.env.TWICTH_CHANNEL.split(','),
+};
+console.log(options)
+const client = new tmi.Client(options);
+client.connect().catch(console.error);
+
+client.on('message', async (channel, tags, message, self) => {
+
+    if (self) return;
+    if (message.toLowerCase() === '!blast') {
+        await gameLeaderboard("https://www.speedrun.com/api/v1/leaderboards/4d70mn17/category/100");
+        displayTwitch(channel, '100%', 'Blast Corps');
+    }
+    if (message.toLowerCase() === '!oot') {
+        await gameLeaderboard("https://www.speedrun.com/api/v1/leaderboards/oot/category/any");
+        displayTwitch(channel, 'ANY%', 'OOT');
+    }
+});
+
+function displayTwitch(channel, category, game) {
+    client.say(channel, `LeaderBoard ${category} ${game}: 1st -> ${nameTopPlayer[0]}: ${timeTopPlayer[0]}, 2nd -> ${nameTopPlayer[1]}: ${timeTopPlayer[1]}, 3rd -> ${nameTopPlayer[2]}: ${timeTopPlayer[2]}`);
+}
